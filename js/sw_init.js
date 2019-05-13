0aa21deb9ea53bda956e52ede47af3131c875503
Aparna Gandhe <33070041+aparnagandhe@users.noreply.github.com>
2019-05-12 22:13:53 -0700

    Update sw_init.js

--- js/sw_init.js
+++ js/sw_init.js
@@ -1,5 +1,6 @@
 // Service Worker Registration
 if('serviceWorker' in navigator) {
+	window.addEventListener('load', function() {
 	navigator.serviceWorker.register('./sw.js')
 	.then((reg) => {
 		console.log('Service Worker Registration Successful', reg);
@@ -7,6 +8,8 @@ if('serviceWorker' in navigator) {
 	.catch((err) => {
 		console.log('Service Worker Registration Unsuccessful', err);
 	})
-} else {
+}
+}); 
+	else {
 	console.log('This browser does not support Service Workers.');
 }
