bc1f0c0d48329e416b368785c9a8fa14ccbccded
Aparna Gandhe <33070041+aparnagandhe@users.noreply.github.com>
2019-05-12 22:00:41 -0700

    Update sw_init.js

--- js/sw_init.js
+++ js/sw_init.js
@@ -2,7 +2,7 @@
  * Service Worker registration
  */
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js').then( () => {
+  navigator.serviceWorker.register('./sw.js').then( () => {
     console.log('Service worker registration worked');
   }).catch(error => {
     console.log('Service worker registration failed:' + error);
